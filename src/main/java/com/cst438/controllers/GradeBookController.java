3436494926493416f9d6a76f3df8c90a2eef50f0
Tom Wooten <73683475+wootent@users.noreply.github.com>
2022-05-27 11:44:43 -0700

    update config

--- src/main/java/com/cst438/controllers/GradeBookController.java
+++ src/main/java/com/cst438/controllers/GradeBookController.java
@@ -32,7 +32,7 @@ import com.cst438.domain.GradebookDTO;
 import com.cst438.services.RegistrationService;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:3000","http://localhost:3001"})
+@CrossOrigin(origins = {"http://localhost:3000","https://cst438gradefe-wooten.herokuapp.com"})
 public class GradeBookController {
 	
 	@Autowired
